5b60cee7a88dc06f75a458b2cc683c8c995eee31
pavans <frontendd6@outlook.com>
2020-12-12 23:53:06 +0530

    python - syntax / variables
    learnt about syntaxs, variables and naming conversions.

---
+++ W3Schools/python3/HelloWorld/helloWorld.py
@@ -0,0 +1,13 @@
+# Hello world example with syntax
+# Python does not really have a syntax for multi line comments
+print("Hello, world!")
+
+# Python has no command for declaring a variable
+text = "Yes, 5 is greater than two"
+
+"""
+This can also be a comment
+"""
+
+if 5 > 2:
+  print(text)
---
+++ W3Schools/python3/Variables/variables.py
@@ -0,0 +1,21 @@
+# A variable is created the moment you first assign a value to it
+# Are case-sensitive
+a = 'letter a'
+print(a)
+
+# They do not need to be declared with any particular type
+# and can even change type after they have been set
+a = 10
+print(a)
+
+# Casting
+a = str('10')
+b = float(20)
+c = int(30)
+
+print(b, a, c)
+print(type(b), type(a), type(c))
+
+# String quotes
+d = 'same string with double quotes as well'
+print(d)
