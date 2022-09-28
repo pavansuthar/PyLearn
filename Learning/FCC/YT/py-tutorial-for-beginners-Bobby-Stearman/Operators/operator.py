1fa6a85c06dbe8fd7c73f1e24502c205ce1ff720
dev-pa1 <frontendd6@outlook.com>
2022-09-28 22:10:17 +0530

    Started Freecodecamp - py for beginners course - Operators

---
+++ Learning/FCC/YT/py-tutorial-for-beginners-Bobby-Stearman/Operators/operator.py
@@ -0,0 +1,36 @@
+# Using py as calculator
+# Using interpreter to take an input and return an output
+# Arthimetic operators
+'''
++ Addition
+- Subtraction
+* Multiplication
+/ Division (returns float which is number type, i.e 10 / 4 is 2.5)
+// Floor division (returns integer version, i.e 10 / 4 is 2) 
+% Modulus (remainder of x / y, i.e remainder of 10 % 4 is 2) | use divmod(a, b)
+** Exponentation (power of) | can also use pow(x, y) instead of x ** y
+'''
+
+# Assignment operators
+'''
+= Equals
+and others ...
+'''
+
+# Number types
+'''
+int (2, 3, 458, 678)
+float (2.2, 3.5, 5.666675678)
+'''
+
+# Built in functions
+'''
+round() (rounds a number with specific no of decimals, i.e round(5.666675678, 2) is 5.67)
+divmod(a, b)
+pow(x, y)
+'''
+
+# Big int
+'''
+py has handly way of making big int's easier to read 4000000000 can be written as 4_000_000_000
+'''
